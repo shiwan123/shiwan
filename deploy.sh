966ed24734c15f288d7ac55da7937f96b82be67b
shiwan123 <591793332@qq.com>
2021-03-12 00:27:54 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:shiwan123/shiwan.git main:gh-pages
+git push -f https://shiwan123@github.com/shiwan123/shiwan.git main:gh-pages
 
 cd -
